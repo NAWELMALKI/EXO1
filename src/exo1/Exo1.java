79d6c874ad07c3d25b86429e89d35130d8f780ce
saidm <saidm@LAPTOP-0U4R9152>
2021-10-04 14:19:50 +0200

    CREATION :MON DEPOT

---
+++ src/exo1/Exo1.java
@@ -0,0 +1,46 @@
+/*
+NAWEL MALKI
+TDB
+TP0 EXERCICE 1
+SEPTEMBRE 2021
+*/
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author saidm
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bonjour");
+        int i;  int maVariable;
+        i=20;   maVariable = 20;
+        i= i+1; maVariable = maVariable +1;
+        
+        System.out.println("Au revoir");
+        
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom?");
+        prenom= sc.nextLine();
+    }    
+    
+    
+}
+
+
